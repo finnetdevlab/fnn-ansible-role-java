f4a719d1c58901b53816760773504e41acd09b05
Doruk Eren Aktaş <dorukerena@gmail.com>
2020-05-12 14:13:02 +0300

    Molecule lint added.

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,9 +5,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts('all')
 
+
 def test_if_java_installed(host):
     cmd = host.run("java -version")
     assert cmd.rc == 0
+
 
 def test_if_JAVA_HOME_present(host):
     cmd = host.run("echo $JAVA_HOME")
