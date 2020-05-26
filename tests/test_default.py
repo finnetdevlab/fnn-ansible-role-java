a428448f815bc9aa73318c8296ec92fe858b9f5c
Doruk Eren Aktaş <dorukerena@gmail.com>
2020-05-26 16:17:58 +0300

    java home path adjusted.

--- tests/test_default.py
+++ tests/test_default.py
@@ -8,8 +8,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_if_java_installed(host):
     cmd = host.run('java -version')
-    assert cmd.rc == 0
+    assert cmd.succeeded
 
 
 def test_if_JAVA_HOME_present(host):
     assert host.file('/etc/profile').contains('JAVA_HOME')
+
+
+def test_if_JAVA_HOME_defined_correctly(host):
+    content = host.file('/etc/profile').content_string
+    matched = [line for line in content.split('\n') if 'JAVA_HOME' in line]
+    java_home = matched[0].split('=')[1]
+    if host.run(java_home + '/jre/bin/java -version').succeeded or \
+            host.run(java_home + '/bin/java -version').succeeded:
+        assert True
+    else:
+        assert False
